b4093ae5796f3e85321d694afdcd1ccb037f65a5
burstlam <burstlam@gmail.com>
2012-08-23 21:41:34 +0800

    Add rootexplorer

--- a10.mk
+++ a10.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml \
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    device/allwinner/a10/prebuilt/app/RootExplorer.apk:system/app/RootExplorer.apk
+
 # Other stuff
 PRODUCT_PACKAGES += librs_jni rild_sun4i power.sun4i
 
